ac3da13e31c50913c412a92604ea00b1eb4cb39d
pbrehm <peter.brehm@outlook.com>
2024-03-24 22:50:36 -0500

    change tmux combo
    since accidental alt space removes focus

--- keyboards/cheapino/pbrehm.c
+++ keyboards/cheapino/pbrehm.c
@@ -7,5 +7,5 @@ enum combos {
 const uint16_t PROGMEM test_combo[] = {LT(1,KC_SPC), LT(4,KC_BSPC), COMBO_END};
 
 combo_t key_combos[] = {
-    [BSPC_SPACE] = COMBO(test_combo, LALT(KC_SPACE)),
+    [BSPC_SPACE] = COMBO(test_combo, LCTL(KC_SPACE)),
 };
